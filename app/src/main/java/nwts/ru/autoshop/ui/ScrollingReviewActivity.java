b9cdc2722d3b9313016cb2faf9ab1d8572ce34c9
lasnwts <lasnwts@gmail.com>
2017-05-31 10:28:20 +0300

    31.05.2017  Add rating

--- app/src/main/java/nwts/ru/autoshop/ui/ScrollingReviewActivity.java
+++ app/src/main/java/nwts/ru/autoshop/ui/ScrollingReviewActivity.java
@@ -6,10 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import nwts.ru.autoshop.R;
+import nwts.ru.autoshop.TODOApplication;
 import nwts.ru.autoshop.setting.PreferenceHelper;
 
 public class ScrollingReviewActivity extends AppCompatActivity {
@@ -17,6 +19,7 @@ public class ScrollingReviewActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private PreferenceHelper preferenceHelper;
     private ImageView productImage;
+    FloatingActionButton fab, fab_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +34,14 @@ public class ScrollingReviewActivity extends AppCompatActivity {
             toolbar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primary_color)));
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setTitle(getResources().getString(R.string.show_title_toolbal_cooment));
+            getSupportActionBar().setTitle(getResources().getString(R.string.show_title_toolbal_cooment)
+                    + TODOApplication.getDetail_productName());
             toolbar.inflateMenu(R.menu.menu_base);
         }
         productImage = (ImageView) findViewById(R.id.imageView_scrollactivity_comment);
         productImage.setImageResource(R.drawable.bigbang);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,7 +49,23 @@ public class ScrollingReviewActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+
+         }
+
+
+    final FloatingActionButton.OnVisibilityChangedListener addVisibilityChanged = new FloatingActionButton.OnVisibilityChangedListener() {
+        public void onShown(final FloatingActionButton fab) {
+            super.onShown(fab);
+            Log.d("ScrollingReviewActivity", "Visibility changed");
+        }
+        public void onHidden(final FloatingActionButton fab) {
+            super.onHidden(fab);
+            Log.d("ScrollingReviewActivity", "Visibility hideen");
+        }
+    };
+
+
+
 
     @Override
     public boolean onSupportNavigateUp() {
